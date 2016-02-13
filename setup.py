ce9409b4b92ba964f2fa756e7e5c4386db79aaa9
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-13 23:25:04 +0100

    First change

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='Monsters of success',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='Prototyping a small app.',
+      author='Kalin Chernev',
+      author_email='kalin.chernev@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
-     )
+      install_requires=['Flask>=0.10.1'],
+      )
