f12a55e59210a962523b890da254edfc7224b2cd
Kalin Chernev <kalin.chernev@gmail.com>
2016-02-14 00:08:48 +0100

    Basic skeleton

--- flaskapp.py
+++ flaskapp.py
@@ -6,13 +6,16 @@ from flask import Flask, request, flash, url_for, redirect, \
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/<path:resource>')
 def serveStaticResource(resource):
     return send_from_directory('static/', resource)
+
 
 @app.route("/test")
 def test():
